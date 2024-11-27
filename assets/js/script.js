bcc41e49f896b3818783813ffda23a004ae38419
MohamedMoustir <itsmoustir@gmail.com>
2024-11-27 02:56:36 +0100

    added cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,29 +23,6 @@ function chenge_formation(soccer) {
 
 
 
-function Attaquant_centra() {
- 
-
-  positionsArray.forEach(positio => {
-    const container = document.getElementById(positio);
-    container.innerHTML += `
-      <div onclick="ajoutePlayers(${positio})" 
-        class="btnCB relative top-[150px] w-[150px] h-[240px] bg-cover bg-center bg-none p-4 z-[2] transition ease-in duration-200 " 
-        style="background-image: url('https://www.futbin.com/design2/img/static/evolutions/placeholder-card-normal.webp');" id="${positio}">
-       <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" class=" text-green-700 relative left-[62%] top-[27%] -translate-x-1/2">
-
-  <path d="M18.6275 41.711L18.3137 41.0298C18.1146 41.1215 17.8854 41.1215 17.6863 41.0298L17.3726 41.711L17.6863 41.0298L1.18627 33.4311C0.920355 33.3087 0.75 33.0427 0.75 32.7499V8.7248C0.75 8.42506 0.928458 8.15411 1.20383 8.03575L17.7038 0.943648C17.8929 0.862375 18.1071 0.862375 18.2962 0.943648L34.7962 8.03575C35.0715 8.15411 35.25 8.42506 35.25 8.7248V32.7499C35.25 33.0427 35.0796 33.3087 34.8137 33.4311L18.3137 41.0298L18.6275 41.711Z" stroke="currentColor" stroke-width="1.5" fill="none"/>
-  
-
-        <h1 class="absolute w-[70px] h-[30px] bottom-[-30px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">GK</h1>
-      </div>`;
-  
-   console.log(container);
-  });
-  
-  
-}
-
 let playerName = document.getElementById("playerName");
 let photosrc = document.getElementById("photosrc");
 let position = document.getElementById("position");
@@ -62,16 +39,20 @@ let physical = document.getElementById("physical")
 let clup = document.getElementById("clup")
 let tablue_players = JSON.parse(localStorage.getItem("players")) || []
 
+localStorage.setItem('playersData', JSON.stringify(tablue_players));
 
+const savedPlayers = JSON.parse(localStorage.getItem('playersData'));
+if (savedPlayers) {
+    tablue_players = savedPlayers;
+    
+}
 
-
-
-function ajoutePlayers(position) {
+function ajoutePlayers(positio) {
   Photo_du_Joueur.classList.toggle("hidden")
   document.getElementById("pop_up_ajoute").classList.toggle("hidden")
-console.log(position);
 
-  const playerCard = document.getElementById(`player-${position}`);
+
+  let playerCard = document.getElementById(`${positio}`);
 
   document.getElementById("btnajout").onclick = function () {
     document.getElementById("pop_up_ajoute").classList.toggle("hidden")
@@ -89,90 +70,23 @@ console.log(position);
       defending: defending.value,
       physical: physical.value
     }
-    tablue_players.push(players)
+    if (players.name && players.photo && players.position) {
+      tablue_players.push(players)
+      localStorage.setItem("players", JSON.stringify(tablue_players));     
+      // afficheJoueurs(playerCard.id);
+      afficheJoueurs();
 
-
+     
+    } else {
+      
+    }
   }
-}
-
-// function afficheJoueurs() {
-//   // Videz les conteneurs des joueurs existants
-//   let allPlayerContainers = document.querySelectorAll(".player-container");
-//   allPlayerContainers.forEach(container => container.innerHTML = "");
-
-//   // Affichez à nouveau les joueurs à partir du tableau
-//   tablue_players.forEach(player => {
-//     const container = document.getElementById();
-//     console.log(player.position);
-    
-//     if (container) {
-//       container.innerHTML += `
-//         <div class="btnCB relative top-[150px] w-[150px] h-[240px] bg-cover bg-center bg-none p-4 z-[2] transition ease-in duration-200" 
-//           style="background-image: url('https://www.futbin.com/design2/img/static/evolutions/placeholder-card-normal.webp');">
-//           <div class="relative flex px-3 text-[#e9cc74]">
-//             <div class="absolute leading-[1.5rem] font-light uppercase py-2 overflow-hidden">
-//               <div class="text-base player-rating"></div>
-//               <div class="text-xs player-position"><span>${player.position}</span></div>
-//               <div class="block w-[1.2rem] h-[12px] my-1 player-nation">
-//                 <img src="${player.nationality}" alt="Nationalité" class="w-full h-full object-contain"/>
-//               </div>
-//               <div class="block w-[1.5rem] h-[25px] player-club">
-//                 <img src="${player.club}" alt="Club" class="w-full h-full object-contain"/>
-//               </div>
-//             </div>
-//             <div class="relative w-[90px] h-[90px] mx-auto overflow-hidden player-picture">
-//               <img src="${player.photo}" alt="${player.name}" class="w-full h-full object-contain"/>
-//             </div>
-//           </div>
-//           <div class="relative">
-//             <div class="block px-1 text-[#e9cc74] w-[80%] mx-auto">
-//               <div class="block text-center text-base uppercase pb-1">${player.name}</div>
-//               <div class="flex justify-center my-1 player-features">
-//                 <div class="items-center border-r border-opacity-10 border-[#e9cc74] px-2">
-//                   <span class="flex text-xs uppercase">
-//                     <div class="mr-1 font-bold">${player.pace}</div>
-//                     <div class="font-light">PAC</div>
-//                   </span>
-//                   <span class="flex text-xs uppercase">
-//                     <div class="mr-1 font-bold">${player.shooting}</div>
-//                     <div class="font-light">SHO</div>
-//                   </span>
-//                   <span class="flex text-xs uppercase">
-//                     <div class="mr-1 font-bold">${player.passing}</div>
-//                     <div class="font-light">PAS</div>
-//                   </span>
-//                 </div>
-//                 <div class="items-center px-2">
-//                   <span class="flex text-xs uppercase">
-//                     <div class="mr-1 font-bold">${player.dribbling}</div>
-//                     <div class="font-light">DRI</div>
-//                   </span>
-//                   <span class="flex text-xs uppercase">
-//                     <div class="mr-1 font-bold">${player.defending}</div>
-//                     <div class="font-light">DEF</div>
-//                   </span>
-//                   <span class="flex text-xs uppercase">
-//                     <div class="mr-1 font-bold">${player.physical}</div>
-//                     <div class="font-light">PHY</div>
-//                   </span>
-//                 </div>
-//               </div>
-//             </div>
-//             <h1 class="absolute w-[70px] h-[30px] bottom-[-60px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">${player.position}</h1>
-//           </div>
-//         </div>`
-//       ;
-//     }
-//   });
-// }
-window.onload = function () {
+ 
   
-  if (localStorage.getItem("players") != null) {
-    tablue_players = JSON.parse(localStorage.getItem("players"));
-  }
 
-  Attaquant_centra();
 }
+
+
 
 
 
@@ -182,26 +96,15 @@ Photo_du_Joueur.addEventListener("wheel", (evnt) => {
   Photo_du_Joueur.scrollLeft += evnt.deltaX;
 
 })
-// photosrc.onclick = function () {
-//   
-// }
-function getSrc(src, alt, position, flag, logo) {
+
+function getSrc(src, alt, flag, logo) {
   photosrc.value = src;
   playerName.value = alt;
-  Position.textContent = position;
   nationality.value = flag;
   clup.value = logo
 
 
 }
-
-
-// console.log(ALphoto);
-
-// function  value_positi(position){
-
-//   return position
-// }
 
 fetch("https://mohamedmoustir.github.io/api.p/")
   .then(result => result.json())
@@ -214,9 +117,8 @@ fetch("https://mohamedmoustir.github.io/api.p/")
 
 
       document.getElementById("Photo_du_Joueur").innerHTML += `
-      <img onclick="getSrc(this.src,this.alt,this.name,this.sizes,this.set)" src="${src.photo}" alt=" ${src.name}" class=" w-[50px] h-[50px] rounded-b-full" name="${src.
-          position
-        }" sizes ="${src.flag}"  >
+      <img onclick="getSrc(this.src,this.alt,this.name,this.sizes)" src="${src.photo}" alt=" ${src.name}" class=" w-[50px] h-[50px] rounded-b-full" name="${src.flag
+        }" sizes="${src.logo}"  >
 `
     })
 
@@ -323,23 +225,21 @@ fetch("https://mohamedmoustir.github.io/api.p/")
 
   })
 
+
   function afficheJoueurs() {
-    // Videz les conteneurs des joueurs existants
-    let allPlayerContainers = document.querySelectorAll(".player-container");
-    allPlayerContainers.forEach(container => container.innerHTML = "");
-  console.log("test");
-  
-    // Affichez à nouveau les joueurs à partir du tableau
+    
+   
+    console.log(tablue_players);
+   
     tablue_players.forEach(player => {
-      const container = document.getElementById(player.position);
-      if (container) {
-        container.innerHTML += `
-          <div class="btnCB relative top-[150px] w-[150px] h-[240px] bg-cover bg-center bg-none p-4 z-[2] transition ease-in duration-200" 
-            style="background-image: url('https://www.futbin.com/design2/img/static/evolutions/placeholder-card-normal.webp');">
+       const container = document.getElementById(player.position);
+      if (container ) {
+        container.innerHTML = `
+         
             <div class="relative flex px-3 text-[#e9cc74]">
               <div class="absolute leading-[1.5rem] font-light uppercase py-2 overflow-hidden">
                 <div class="text-base player-rating"></div>
-                <div class="text-xs player-position"><span>${player.position}</span></div>
+                <div class="text-xs player-position"><span>${player.position.slice(7)}</span></div>
                 <div class="block w-[1.2rem] h-[12px] my-1 player-nation">
                   <img src="${player.nationality}" alt="Nationalité" class="w-full h-full object-contain"/>
                 </div>
@@ -357,11 +257,12 @@ fetch("https://mohamedmoustir.github.io/api.p/")
                 <div class="flex justify-center my-1 player-features">
                   <div class="items-center border-r border-opacity-10 border-[#e9cc74] px-2">
                     <span class="flex text-xs uppercase">
-                      <div class="mr-1 font-bold">${player.pace}</div>
+                      <div class="mr-1 font-bold">${player.flag}</div>
                       <div class="font-light">PAC</div>
                     </span>
                     <span class="flex text-xs uppercase">
-                      <div class="mr-1 font-bold">${player.shooting}</div>
+                      <div class="mr-1 font-bold">${player.dribbling
+                        }</div>
                       <div class="font-light">SHO</div>
                     </span>
                     <span class="flex text-xs uppercase">
@@ -385,11 +286,16 @@ fetch("https://mohamedmoustir.github.io/api.p/")
                   </div>
                 </div>
               </div>
-              <h1 class="absolute w-[70px] h-[30px] bottom-[-60px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">${player.position}</h1>
+              <h1 class="absolute w-[70px] h-[30px] bottom-[-60px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">${player.position.slice(7)}</h1>
             </div>
-          </div>
+      
         `;
       }
+      console.log(player.pace);
+      
     });
+  
+    
   }
+  
   afficheJoueurs();
