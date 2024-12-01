a449a277a1e81135fdc74cf4c0fd186c18b68634
MohamedMoustir <itsmoustir@gmail.com>
2024-12-01 16:07:01 +0100

    Update player position functionality and commit changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,6 +147,8 @@ function ajoutePlayers(positio) {
         localStorage.setItem("players", JSON.stringify(tablue_players));
         afficheJoueurs();
         clearFields();
+      nam(position_get)
+
       }
 
 
@@ -274,7 +276,6 @@ function afficheJoueurs() {
                   </div>
                 </div>
               </div>
-              <h1 class="absolute w-[70px] h-[30px] bottom-[-60px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">${player.position.slice(7)}</h1>
           <div >
  <div id="btn" class="  absolute -top-[65%]  left-[100%] transform -translate-x-1/2 justify-between items-center mt-4 space-x-4 ">
     <svg onclick="update(${player.id})" class="w-6 h-6 text-gray-300 hover:text-[#e9cc74] cursor-pointer transition duration-300 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
@@ -356,10 +357,9 @@ function update(i) {
     document.getElementById("pop_up_ajoute").classList.toggle("hidden");
     Photo_du_Joueur.classList.toggle("hidden");
 
-    if (container && !player.position) {
+    if (container) {
       container.innerHTML = ` <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" class="text-green-700 relative left-[62%] top-[27%] -translate-x-1/2">
               <path d="M18.6275 41.711L18.3137 41.0298C18.1146 41.1215 17.8854 41.1215 17.6863 41.0298L17.3726 41.711L17.6863 41.0298L1.18627 33.4311C0.920355 33.3087 0.75 33.0427 0.75 32.7499V8.7248C0.75 8.42506 0.928458 8.15411 1.20383 8.03575L17.7038 0.943648C17.8929 0.862375 18.1071 0.862375 18.2962 0.943648L34.7962 8.03575C35.0715 8.15411 35.25 8.42506 35.25 8.7248V32.7499C35.25 33.0427 35.0796 33.3087 34.8137 33.4311L18.3137 41.0298L18.6275 41.711Z" stroke="currentColor" stroke-width="1.5" fill="none"/>
-            <h1 class="absolute w-[70px] h-[30px] bottom-[-30px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">${player.position.slice(7)}</h1>
             </svg> `;
     }
   }
@@ -378,7 +378,6 @@ function removePlayer(i) {
   if (container) {
     container.innerHTML = ` <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" class="text-green-700 relative left-[62%] top-[27%] -translate-x-1/2">
             <path d="M18.6275 41.711L18.3137 41.0298C18.1146 41.1215 17.8854 41.1215 17.6863 41.0298L17.3726 41.711L17.6863 41.0298L1.18627 33.4311C0.920355 33.3087 0.75 33.0427 0.75 32.7499V8.7248C0.75 8.42506 0.928458 8.15411 1.20383 8.03575L17.7038 0.943648C17.8929 0.862375 18.1071 0.862375 18.2962 0.943648L34.7962 8.03575C35.0715 8.15411 35.25 8.42506 35.25 8.7248V32.7499C35.25 33.0427 35.0796 33.3087 34.8137 33.4311L18.3137 41.0298L18.6275 41.711Z" stroke="currentColor" stroke-width="1.5" fill="none"/>
-          <h1 class="absolute w-[70px] h-[30px] bottom-[-30px] bg-[#1e1d1d] left-1/2 transform -translate-x-1/2 text-center text-white rounded-full">${i.slice(7)}</h1>
           </svg> `;
   }
 
@@ -387,7 +386,7 @@ function removePlayer(i) {
 afficheJoueurs();
 
 
-function Calcul_de_la_Chimie(player) {
+function Calcul_de_la_Chimie(player){
 
   let count = tablue_players.filter(p => p.nationalitytext === player.nationalitytext).length;
   let somme = 0;
@@ -410,9 +409,14 @@ function Calcul_de_la_Chimie(player) {
 
 
 function nam(position_get) {
+  let somme =100
   if (position.value === position_get) {
-    console.log("yess");
-
+   somme +=10
+  }else{
+   somme -=10
+    
   }
+  console.log(somme);
+  
 }
 
